3eaa6e272f81d7a750beee455cd350ffc3740909
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-06-06 16:45:18 +0200

    added retrofit dependencies for API connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Retrofit with Scalar Converter : enables Retrofit to return the JSON result as a String
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 }
