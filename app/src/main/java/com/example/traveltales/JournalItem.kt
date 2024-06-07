b579f9e34f4686c06b6d9836f951650a3be3fe5f
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-06-07 15:32:17 +0200

    made journal button text bigger and bold

--- app/src/main/java/com/example/traveltales/JournalItem.kt
+++ app/src/main/java/com/example/traveltales/JournalItem.kt
@@ -9,8 +9,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
 @Composable
 fun JournalItem(journalId: Int, name: String, themeId: Int?, onClick: () -> Unit) {
     Button(
@@ -21,6 +22,10 @@ fun JournalItem(journalId: Int, name: String, themeId: Int?, onClick: () -> Unit
             .fillMaxWidth()
             .padding(vertical = 8.dp)
     ) {
-        Text(text = "$name")
+        Text(
+            text = "$name",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
